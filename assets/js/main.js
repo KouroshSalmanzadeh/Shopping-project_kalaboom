1edd3bff479d7bbf88d7d33a737cfab706902715
KouroshSalmanzadeh <kouroshsalmanzadeh@gmail.com>
2023-12-22 23:55:00 +0330

    Crate navbar for desktop #2

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// // اسکرول را گوش کنید
+// window.addEventListener('scroll', function() {
+//     // ارتفاع اسکرول شده را دریافت کنید
+//     var scrollPosition = window.scrollY;
+  
+//     // منوی ثابت
+//     var fixedMenu = document.getElementById('fixed-menu');
+  
+//     // اگر اسکرول به اندازه کافی پایین رفته باشد، منو را فیکس کنید
+//     if (scrollPosition > 50) {
+//       fixedMenu.style.backgroundColor = '#555'; // تغییرات رنگ می‌تواند به سلیقه شما باشد
+//     } else {
+//       fixedMenu.style.backgroundColor = '#333'; // رنگ اصلی منو
+//     }
+//   });
+
+// function menuHover() {
+//     document.querySelector()
+// }
